2c31ae326741e08efbe316d34732644f3cd88ae6
Oleshkooo <olehchoma@gmail.com>
2022-05-30 13:53:01 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss'
+
+export default function App() {
+    return (
+        <div className="app">
+            <h1>
+                <i className="fa-brands fa-react"></i>
+                App
+            </h1>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './Icons/icons/all.min.css'
+import './Icons/icons/svg-with-js.min.css'
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />)
